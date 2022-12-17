9002df1ea1b0fb1b9d1b1bf192f5c0c0932315da
Ggosh1 <radiging@gmail.com>
2022-12-17 18:04:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+import sqlite3
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        cur = self.con.cursor()
+        res = cur.execute("""SELECT id, sort_name, degree_of_roasting, is_ground, description, cost, volume FROM coffee""")
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['Время, с', 'Скорость, зн/мин', 'Точность, %', 'Дата и время'])
+        self.tableWidget.resizeColumnsToContents()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
